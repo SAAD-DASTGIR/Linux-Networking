6d03320255f68a631a07c33221e384f540d24f7b
Saad Dastgir <129270933+SAAD-DASTGIR@users.noreply.github.com>
2024-06-14 18:21:02 +0500

    Create commands.sh

---
+++ commands.sh
@@ -0,0 +1,16 @@
+netstat
+ifconfig
+ping google.com
+traceroute google.com
+tracepath saaddastgir.co
+mtr admin.saaddastgir.co
+nslookup saaddastgir.co
+telnet saaddastgir.co 80
+whois google.com
+dig google.com
+nmap google.com
+curl 
+wget
+tcpdump 
+iwconfig
+arp
